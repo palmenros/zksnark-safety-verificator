021884ad780af9070a9e6198b958286ebcb8dca3
Pedro Palacios Almendros <palmenros@gmail.com>
2023-03-24 11:18:57 +0100

    Implement basic graph drawing

--- verification/src/input_data.rs
+++ verification/src/input_data.rs
@@ -1,42 +1,59 @@
-use std::{collections::{HashMap, LinkedList}, io};
-use std::fs::File;
-use std::error::Error;
-use std::io::BufRead;
-use std::path::Path;
-use itertools::Itertools;
-use num_bigint_dig::BigInt;
-use serde_json::Value;
 use circom_algebra::algebra::Constraint;
 use circom_algebra::constraint_storage::ConstraintStorage;
-use serde::{Serialize, Deserialize};
-
+use itertools::Itertools;
+use num_bigint_dig::BigInt;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufRead;
+use std::path::Path;
+use std::{
+    collections::{HashMap, LinkedList},
+    io,
+};
 
 pub fn parse_constraint_list(path: &Path) -> Result<ConstraintStorage, Box<dyn Error>> {
     let f = File::open(path)?;
-    let data : Value = serde_json::from_reader(f)?;
+    let data: Value = serde_json::from_reader(f)?;
 
+    let o = data
+        .as_object()
+        .ok_or("constraint.json main value is not an object")?;
+    let json_constraint_list = o
+        .get("constraints")
+        .ok_or("constraint.json main object does not contain a constraints array")?;
 
-    let o = data.as_object().ok_or("constraint.json main value is not an object")?;
-    let json_constraint_list = o.get("constraints").ok_or("constraint.json main object does not contain a constraints array")?;
-
-    let v = json_constraint_list.as_array().ok_or("constraint.json 'constraints' value is not an array")?;
+    let v = json_constraint_list
+        .as_array()
+        .ok_or("constraint.json 'constraints' value is not an array")?;
     let mut storage = ConstraintStorage::new();
 
     for val in v {
         // Read one constraint
-        let arr = val.as_array().ok_or("constraint.json contains a non-array in constraint list")?;
+        let arr = val
+            .as_array()
+            .ok_or("constraint.json contains a non-array in constraint list")?;
         if arr.len() != 3 {
             return Err("Constraint in constraint.json has more than 3 terms".into());
         }
 
-        let maybe_cs : Result<Vec<_>, _> = arr.into_iter().map(|x| -> Result<HashMap<usize, BigInt>, Box<dyn Error>> {
-            let m = x.as_object().ok_or("Constraint in 'constraint.json' has a non-object")?;
-            m.into_iter()
-                .map(|(k, v)| -> Result<(usize, BigInt), Box<dyn Error>> {
-                    let s = v.as_str().ok_or("Coefficient in 'constraint.json' is not a string")?;
-                    Ok((k.parse::<usize>()?, s.parse::<BigInt>()?))
-                }).collect()
-        }).collect();
+        let maybe_cs: Result<Vec<_>, _> = arr
+            .into_iter()
+            .map(|x| -> Result<HashMap<usize, BigInt>, Box<dyn Error>> {
+                let m = x
+                    .as_object()
+                    .ok_or("Constraint in 'constraint.json' has a non-object")?;
+                m.into_iter()
+                    .map(|(k, v)| -> Result<(usize, BigInt), Box<dyn Error>> {
+                        let s = v
+                            .as_str()
+                            .ok_or("Coefficient in 'constraint.json' is not a string")?;
+                        Ok((k.parse::<usize>()?, s.parse::<BigInt>()?))
+                    })
+                    .collect()
+            })
+            .collect();
 
         let (A, B, C) = maybe_cs?.into_iter().collect_tuple().unwrap();
         storage.add_constraint(Constraint::new(A, B, C));
@@ -49,13 +66,20 @@ pub type Witness = HashMap<usize, BigInt>;
 
 pub fn parse_witness(path: &Path) -> Result<Witness, Box<dyn Error>> {
     let f = File::open(path)?;
-    let data : Value = serde_json::from_reader(f)?;
+    let data: Value = serde_json::from_reader(f)?;
 
-    let o = data.as_object().ok_or("witness.json main value is not an object")?;
-    let map = o.into_iter().map(|(k, v)| -> Result<(usize, BigInt), Box<dyn Error>> {
-        let s = v.as_str().ok_or("witness.json has a witness value that is not a string")?;
-        Ok((k.parse::<usize>()?, s.parse::<BigInt>()?))
-    }).collect::<Result<Witness, Box<dyn Error>>>()?;
+    let o = data
+        .as_object()
+        .ok_or("witness.json main value is not an object")?;
+    let map = o
+        .into_iter()
+        .map(|(k, v)| -> Result<(usize, BigInt), Box<dyn Error>> {
+            let s = v
+                .as_str()
+                .ok_or("witness.json has a witness value that is not a string")?;
+            Ok((k.parse::<usize>()?, s.parse::<BigInt>()?))
+        })
+        .collect::<Result<Witness, Box<dyn Error>>>()?;
 
     Ok(map)
 }
@@ -68,7 +92,13 @@ pub fn parse_signal_name_map(path: &Path) -> Result<SignalNameMap, Box<dyn Error
 
     for maybe_line in io::BufReader::new(f).lines() {
         let line = maybe_line.unwrap();
-        let (id, _, _, name) = line.split(',').collect_tuple().ok_or("Invalid number of entries per line in 'circuit_signals.sym'")?;
+        let (id, _, _, fully_qualified_name) = line
+            .split(',')
+            .collect_tuple()
+            .ok_or("Invalid number of entries per line in 'circuit_signals.sym'")?;
+
+        // Remove first component path from name, that is, remove the initial "main."
+        let (_, name) = fully_qualified_name.split_once(".").unwrap();
         map.insert(id.parse::<usize>()?, name.to_string());
     }
 
@@ -85,32 +115,33 @@ pub struct TreeConstraints {
     pub number_outputs: usize,
     pub number_signals: usize,
     pub initial_signal: usize,
-    pub are_double_arrow: Vec<(usize, usize)>, // first number constraint, second number assigned signal
+    pub are_double_arrow: Vec<(usize, usize)>,
+    // first number constraint, second number assigned signal
     pub subcomponents: LinkedList<TreeConstraints>,
 }
 
 pub fn parse_tree_constraints(path: &Path) -> Result<TreeConstraints, Box<dyn Error>> {
     let f = File::open(path)?;
-    let constraints : TreeConstraints = serde_json::from_reader(f)?;
+    let constraints: TreeConstraints = serde_json::from_reader(f)?;
 
     Ok(constraints)
 }
 
 pub fn print_constraint(c: &Constraint<usize>) {
     println!("Linear Expression A:");
-    for c2 in c.a(){
-        println!("     Signal: {:}",c2.0);
-        println!("     Value : {:}",c2.1.to_string());
+    for c2 in c.a() {
+        println!("     Signal: {:}", c2.0);
+        println!("     Value : {:}", c2.1.to_string());
     }
     println!("Linear Expression B:");
-    for c2 in c.b(){
-        println!("     Signal: {:}",c2.0);
-        println!("     Value : {:}",c2.1.to_string());
+    for c2 in c.b() {
+        println!("     Signal: {:}", c2.0);
+        println!("     Value : {:}", c2.1.to_string());
     }
     println!("Linear Expression C: ");
-    for c2 in c.c(){
-        println!("     Signal: {:}",c2.0);
-        println!("     Value : {:}",c2.1.to_string());
+    for c2 in c.c() {
+        println!("     Signal: {:}", c2.0);
+        println!("     Value : {:}", c2.1.to_string());
     }
 }
 
--- verification/src/tree_constraint_graph_printer.rs
+++ verification/src/tree_constraint_graph_printer.rs
@@ -1,34 +1,70 @@
-use std::path::Path;
+use crate::{SignalNameMap, TreeConstraints};
+use graphviz_rust::attributes::{color_name, shape, NodeAttributes};
+use graphviz_rust::cmd::Format;
 use graphviz_rust::dot_generator::*;
 use graphviz_rust::dot_structures::*;
 use graphviz_rust::printer::{DotPrinter, PrinterContext};
+use graphviz_rust::{exec, print};
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io::Write;
-use graphviz_rust::attributes::{color_name, NodeAttributes, shape};
-use graphviz_rust::cmd::Format;
-use graphviz_rust::{exec, print};
-use crate::{SignalNameMap, TreeConstraints};
+use std::path::Path;
+use circom_algebra::constraint_storage::ConstraintStorage;
 
-fn construct_graph_from_tree_constraint(tree_constraints: &TreeConstraints, signal_name_map: &SignalNameMap) -> Graph {
-    let mut g = graph!(id!("id"));
+fn construct_graph_from_tree_constraint(
+    tree_constraints: &TreeConstraints,
+    signal_name_map: &SignalNameMap,
+    storage: &ConstraintStorage,
+) -> Graph {
+    let mut g = graph!(di id!("id"));
 
     // Outputs
     for idx in 0..tree_constraints.number_outputs {
         let s = idx + tree_constraints.initial_signal;
-        g.add_stmt(Stmt::Node(node!(s.to_string(); attr!("label", esc signal_name_map.get(&s).unwrap()))));
+        g.add_stmt(Stmt::Node(
+            node!(s.to_string();
+                attr!("label", esc signal_name_map.get(&s).unwrap()),
+                attr!("color", "red"),
+                attr!("shape", "Mdiamond")
+            ),
+        ));
+
+        // Output edge going to nowhere
+        let tmp_str = format!("output_dummy_{idx}");
+        g.add_stmt(Stmt::Node(node!(tmp_str; attr!("shape", "none"), attr!("label", esc ""))));
+        g.add_stmt(Stmt::Edge(edge!(node_id!(s.to_string()) => node_id!(tmp_str) )));
     }
 
     // Inputs
     for idx in 0..tree_constraints.number_inputs {
-        let s = idx + tree_constraints.number_outputs +  tree_constraints.initial_signal;
-        g.add_stmt(Stmt::Node(node!(s.to_string(); attr!("label", esc signal_name_map.get(&s).unwrap()))));
+        let s = idx + tree_constraints.number_outputs + tree_constraints.initial_signal;
+        g.add_stmt(Stmt::Node(
+            node!(s.to_string();
+                attr!("label", esc signal_name_map.get(&s).unwrap()),
+                attr!("color", "orange"),
+                attr!("shape", "Mdiamond")
+            ),
+        ));
+
+        // Input edge coming from nowhere
+        let tmp_str = format!("input_dummy_{idx}");
+        g.add_stmt(Stmt::Node(node!(tmp_str; attr!("shape", "none"), attr!("label", esc ""))));
+        g.add_stmt(Stmt::Edge(edge!(node_id!(tmp_str) => node_id!(s.to_string()))));
     }
 
     // Intermediates
-    for idx in 0..(tree_constraints.number_signals - tree_constraints.number_outputs - tree_constraints.number_inputs) {
-        let s = idx + tree_constraints.number_outputs + tree_constraints.number_inputs +  tree_constraints.initial_signal;
-        g.add_stmt(Stmt::Node(node!(s.to_string(); attr!("label", esc signal_name_map.get(&s).unwrap()))));
+    let number_intermediates = tree_constraints.number_signals - tree_constraints.number_outputs
+        - tree_constraints.number_inputs;
+
+    for idx in 0..number_intermediates {
+        let s = idx
+            + tree_constraints.number_outputs
+            + tree_constraints.number_inputs
+            + tree_constraints.initial_signal;
+        g.add_stmt(Stmt::Node(
+            node!(s.to_string(); attr!("label", esc signal_name_map.get(&s).unwrap())),
+        ));
     }
 
     // Components
@@ -37,20 +73,39 @@ fn construct_graph_from_tree_constraint(tree_constraints: &TreeConstraints, sign
     for c in &tree_constraints.subcomponents {
         let mut v = Vec::<Stmt>::new();
 
+        let dummy_node_str = format!("dummy_{cmp_index}");
+
+        // Dummy point for edges
+        v.push(Stmt::Node(node!(dummy_node_str;
+            attr!("shape", "point")
+        )));
+
         // Subcomponent outputs
         for idx in 0..c.number_outputs {
             let s = idx + c.initial_signal;
             v.push(Stmt::Node(node!(s.to_string();
-                attr!("label", esc signal_name_map.get(&s).unwrap())
+                attr!("label", esc signal_name_map.get(&s).unwrap()),
+                attr!("color", "green")
             )));
+
+            // Edge
+            v.push(Stmt::Edge(edge!(node_id!(dummy_node_str) => node_id!(s.to_string()))));
         }
 
         // Subcomponent inputs
 
         for idx in 0..c.number_inputs {
-            let s = idx + c.number_outputs +  c.initial_signal;
-            v.push(Stmt::Node(node!(s.to_string();
-                attr!("label", esc signal_name_map.get(&s).unwrap())
+            let s = idx + c.number_outputs + c.initial_signal;
+            let i = node!(s.to_string();
+                attr!("label", esc signal_name_map.get(&s).unwrap()),
+                attr!("color", "blue")
+            );
+
+            v.push(Stmt::Node(i));
+
+            // Edge
+            v.push(Stmt::Edge(edge!(node_id!(s.to_string()) => node_id!(dummy_node_str);
+                  attr!("dir", "none")
             )));
         }
 
@@ -60,8 +115,8 @@ fn construct_graph_from_tree_constraint(tree_constraints: &TreeConstraints, sign
         subgraph.stmts.push(Stmt::Attribute(attr!("color", "lightgrey")));
         subgraph.stmts.push(Stmt::Attribute(attr!("label", esc c.template_name)));
         subgraph.stmts.push(Stmt::GAttribute(GraphAttributes::Node(vec![
-                attr!("style", "filled"),
-                attr!("color", "white")
+            attr!("style", "filled"),
+            attr!("fillcolor", "white"),
         ])));
 
         subgraph.stmts.append(&mut v);
@@ -71,21 +126,46 @@ fn construct_graph_from_tree_constraint(tree_constraints: &TreeConstraints, sign
         cmp_index += 1;
     }
 
+    // Constraints
+
+    // Double arrow constraints
+    for (cnt, assigned_signal) in &tree_constraints.are_double_arrow {
+        let c = storage.read_constraint(*cnt).unwrap();
+        let mut sources = c.take_cloned_signals();
+        sources.remove(assigned_signal);
+
+        // TODO: Expand to assignment from multiple sources
+        assert_eq!(sources.len(), 1);
+
+        for source in sources {
+            g.add_stmt(Stmt::Edge(edge!(
+                node_id!(source.to_string()) => node_id!(assigned_signal.to_string());
+                attr!("label", esc " <=="),
+                attr!("fontname", "Courier"),
+                attr!("color", "red")
+            )));
+        }
+    }
+
+    // TODO: Handle non-double arrow signals
+    assert_eq!(tree_constraints.no_constraints, tree_constraints.are_double_arrow.len());
+
     return g;
 }
 
-pub fn print_tree_constraint_graph(tree_constraints: &TreeConstraints, signal_name_map: &SignalNameMap, path: &Path) -> Result<(), Box<dyn Error>> {
-    let g = construct_graph_from_tree_constraint(tree_constraints, signal_name_map);
+pub fn print_tree_constraint_graph(
+    tree_constraints: &TreeConstraints,
+    signal_name_map: &SignalNameMap,
+    storage: &ConstraintStorage,
+    path: &Path,
+) -> Result<(), Box<dyn Error>> {
+    let g = construct_graph_from_tree_constraint(tree_constraints, signal_name_map, storage);
 
     // Debug print of Graphviz code
     let s = print(g.clone(), &mut PrinterContext::default());
     println!("{}", s);
 
-    let graph_svg = exec(
-        g,
-        &mut PrinterContext::default(),
-        vec![Format::Svg.into()],
-    )?;
+    let graph_svg = exec(g, &mut PrinterContext::default(), vec![Format::Svg.into()])?;
 
     let mut f = File::create(path)?;
     f.write_all(graph_svg.as_bytes())?;
