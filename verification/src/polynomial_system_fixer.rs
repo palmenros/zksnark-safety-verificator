357432398c3c8a5d138db6756ba43e936a6d35c6
Pedro Palacios Almendros <palmenros@gmail.com>
2023-04-26 20:42:42 +0200

    Add support for indexed variable name for better CAS support

--- verification/src/polynomial_system_fixer.rs
+++ verification/src/polynomial_system_fixer.rs
@@ -10,13 +10,25 @@ use num_bigint_dig::BigInt;
 use num_traits::One;
 use std::collections::{BTreeSet, HashMap};
 
+// This enum controls how each signal should be displayed: either as its name (which is human
+//  readable but may cause problems with Computer Algebra Systems), or as a signal index (which is
+//  not easily human readable but can be safely used by Computer Algebra Systems).
+#[derive(Copy, Clone)]
+enum SignalDisplayKind {
+    Name,
+    Index,
+}
+
 pub fn print_polynomial_system(
     pol_system: &PolynomialSystemFixedSignal,
     context: &InputDataContextView,
 ) {
+    let display_kind = SignalDisplayKind::Name;
+
     println!("\nConstraints: ");
+
     for constraint in &pol_system.constraints {
-        println!("{} = 0", get_constraint_polynomial(constraint, context));
+        println!("{} = 0", get_constraint_polynomial(constraint, context, display_kind));
     }
 
     let signals_to_fix_name_vec: Vec<String> = pol_system
@@ -29,27 +41,36 @@ pub fn print_polynomial_system(
     println!("Prohibition constraint: ");
     println!(
         "{} = 0",
-        get_prohibition_witness_polynomial(&pol_system.signals_to_fix, context)
+        get_prohibition_witness_polynomial(&pol_system.signals_to_fix, context, display_kind)
     );
 }
 
 fn get_prohibition_witness_polynomial(
     signals_to_fix: &BTreeSet<SignalIndex>,
     context: &InputDataContextView,
+    display_kind: SignalDisplayKind,
 ) -> String {
     let str: String = Itertools::intersperse(
         signals_to_fix
             .iter()
             .enumerate()
             .map(|(i, signal_idx)| -> String {
-                let signal_name = &context.signal_name_map[signal_idx];
+                let indexed_signal_kind = format!("x_{}", signal_idx);
+                let signal_name = match display_kind {
+                    SignalDisplayKind::Name => {
+                        &context.signal_name_map[signal_idx]
+                    }
+                    SignalDisplayKind::Index => {
+                        &indexed_signal_kind
+                    }
+                };
                 let witness_value = &context.witness[signal_idx];
 
                 format!("(({} - {})*u_{} - 1)", signal_name, witness_value, i)
             }),
         " * ".to_string(),
     )
-    .collect();
+        .collect();
 
     str
 }
@@ -57,6 +78,7 @@ fn get_prohibition_witness_polynomial(
 fn get_constraint_polynomial(
     constraint: &Constraint<usize>,
     context: &InputDataContextView,
+    display_kind: SignalDisplayKind,
 ) -> String {
     // We assume that the constraints are fixed (called Constraint::fix_constraint)
     // TODO: Check that fix_constraint is called
@@ -67,12 +89,12 @@ fn get_constraint_polynomial(
 
     if a.is_empty() || b.is_empty() {
         //  Only linear constraint c
-        linear_term_to_string(c, context, false)
+        linear_term_to_string(c, context, false, display_kind)
     } else {
         // TODO: Do not print the + symbol if
-        let a_str = linear_term_to_string(a, context, true);
-        let b_str = linear_term_to_string(b, context, true);
-        let c_str = linear_term_to_string(c, context, false);
+        let a_str = linear_term_to_string(a, context, true, display_kind);
+        let b_str = linear_term_to_string(b, context, true, display_kind);
+        let c_str = linear_term_to_string(c, context, false, display_kind);
 
         if c_str.starts_with('-') {
             format!(
@@ -92,6 +114,7 @@ fn linear_term_to_string(
     linear_term: &HashMap<usize, BigInt>,
     context: &InputDataContextView,
     surround_with_parenthesis: bool,
+    display_kind: SignalDisplayKind,
 ) -> String {
     if linear_term.is_empty() {
         return "0".to_string();
@@ -102,11 +125,20 @@ fn linear_term_to_string(
     let s: String = linear_term
         .iter()
         .sorted_by_key(|(&idx, _)| idx)
-        .map(|(&idx, coeff)| -> String {
-            if idx == ArithmeticExpression::<usize>::constant_coefficient() {
+        .map(|(&signal_idx, coeff)| -> String {
+            if signal_idx == ArithmeticExpression::<usize>::constant_coefficient() {
                 coefficient_to_string(coeff, prime)
             } else {
-                let signal_name = context.signal_name_map.get(&idx).unwrap();
+                let indexed_signal_name = format!("x_{}", signal_idx);
+                let signal_name = match display_kind {
+                    SignalDisplayKind::Name => {
+                        &context.signal_name_map[&signal_idx]
+                    }
+                    SignalDisplayKind::Index => {
+                        &indexed_signal_name
+                    }
+                };
+
                 if coeff.is_one() {
                     signal_name.clone()
                 } else if coeff.eq(&(prime - &BigInt::one())) {
